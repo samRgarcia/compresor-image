92dfb40ac3acae1a679cf6d57356132fc9d8276d
samRgarcia <samuelrivas18@gmail.com>
2021-02-20 22:22:50 -0600

    Primer commit

---
+++ src/componentes/ImageCompresor.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import imageCompression from "browser-image-compression";
+import Card from "react-bootstrap/Card";
+
+
+const ImageCompressor = () => {
+    const [state, setState] = React.useState({
+        compressedLink:
+            "http://navparivartan.in/wp-content/uploads/2018/11/placeholder.png",
+        originalImage: "",
+        originalLink: "",
+        clicked: false,
+        uploadImage: false
+    })
+    const [clicked, setClicked] = React.useState(false);
+
+    const handl = e => {
+        const imageFile = e.target.files[0];
+        setState({
+            ...state,
+            originalLink: URL.createObjectURL(imageFile),
+            originalImage: imageFile,
+            outputFileName: imageFile.name,
+            uploadImage: true
+        })
+    }
+
+    const click = e => {
+        e.preventDefault();
+        const options = {
+            maxSizeMB: 1,
+            maxWidthOrHeight: 500,
+            useWebWorker: true,
+        }
+        if (options.maxSizeMB >= state.originalImage.size / 1024) {
+            alert("Image is too small, can't be Compressed!")
+            return 0;
+        }
+
+        let output;
+        imageCompression(state.originalImage, options)
+            .then(x => {
+                output = x;
+                const downloadLink = URL.createObjectURL(output);
+                setState({
+                    ...state,
+                    compressedLink: downloadLink
+                });
+            });
+        setClicked(true);
+        return 1
+    }
+
+    return (
+        <div className={'m-5'}>
+        <div className={"text-light text-center"}>
+            <h1>Three Simple steps</h1>
+            <h3>1. Upload Image</h3>
+            <h3>2. Click on Compress</h3>
+            <h3>3. Download Compressed Image</h3>
+        </div>
+
+        <div className={'row mt-5'}>
+            <div className={'col-xl-4 col-lg-4 col-md-12 col-sm-12'}>
+                {
+                    state.uploadImage ? (
+                        <Card.Img
+                            className={"ht"}
+                            variant={"top"}
+                            src={state.originalLink}
+                        />
+                    ) : (
+                        <Card.Img
+                            className={"ht"}
+                            variant={"top"}
+                            src={"http://navparivartan.in/wp-content/uploads/2018/11/placeholder.png"}
+                        />
+                    )}
+                <div className={'d-flex justify-content-center'}>
+                    <input
+                        type={'file'}
+                        accept={'image/*'}
+                        className={'mt-2 btn btn-dark w-75'}
+                        onChange={e => handl(e)}
+                    />
+                </div>
+            </div>
+            <div
+                    className={"col-xl-4 col-lg-4 col-md-12 mb-5 mt-5 col-sm-12 d-flex justify-content-center align-items-baseline"}>
+                    <br/>
+                    {
+                        state.outputFileName ? (
+                            <button
+                                type={'button'}
+                                className={'btn btn-dark'}
+                                onClick={e => click(e)}
+                            >
+                                Compress
+                            </button>
+                        ) : null
+                    }
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 mt-3">
+                    <Card.Img
+                        variant={'top'}
+                        src={state.compressedLink}
+                    >
+                    </Card.Img>
+                    {
+                        clicked ?
+                            (<div className="d-flex justify-content-center">
+                                <a
+                                    className="mt-2 btn btn-dark w-75"
+                                    href={state.compressedLink}
+                                    download={state.outputFileName}
+                                >Download</a>
+                            </div>) : null
+                    }
+                </div>
+        </div>
+    </div>)
+}
+
+export default ImageCompressor;
